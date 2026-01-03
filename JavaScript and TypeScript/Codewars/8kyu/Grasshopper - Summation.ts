424cb166d67f7a03e82bc0482bd46534724248ae
Andy <su27t34@gmail.com>
2026-01-03 22:20:49 +0800

    update js & ts practice

---
+++ JavaScript and TypeScript/Codewars/8kyu/Grasshopper - Summation.ts
@@ -0,0 +1,8 @@
+// Grasshopper - Summation
+// Write a program that finds the summation of every number from 1 to num (both inclusive). The number will always be a positive integer greater than 0. Your function only needs to return the result, what is shown between parentheses in the example below is how you reach that result and it's not part of it, see the sample tests.
+// https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/typescript
+export const summation = (num:number)=> { 
+    let result: number;
+    result = (1 + num) * num / 2;
+    return result
+}
