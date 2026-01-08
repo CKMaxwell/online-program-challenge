07aac6004638d1e07deceaeb41d1f4ad94a75feb
Andy <su27t34@gmail.com>
2026-01-09 01:43:40 +0800

    update js & ts practice on codewars

---
+++ JavaScript and TypeScript/Codewars/8kyu/Even or Odd.js
@@ -0,0 +1,10 @@
+// Even or Odd
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/typescript
+function evenOrOdd(number) {
+    if (number % 2 == 0) {
+        return 'Even'
+    } else if (number % 2 !== 0) {
+        return 'Odd'
+    }
+}
