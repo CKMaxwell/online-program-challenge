7c71d4d49d56670a8d72736e41696326c333f3bd
Andy <su27t34@gmail.com>
2026-02-23 19:50:49 +0800

    update js & ts practice

--- JavaScript and TypeScript/Codewars/8kyu/Square(n) Sum.ts
+++ JavaScript and TypeScript/Codewars/8kyu/Square(n) Sum.ts
@@ -2,7 +2,8 @@
 /*
 Complete the square sum function so that it squares each number passed into it and then sums the results together.
 */
-function squareSum(numbers) {
+// https://www.codewars.com/kata/515e271a311df0350d00000f/train/typescript
+export function squareSum(numbers: number[]): number {
   let ans = 0;
   for (let element of numbers) {
     ans += element ** 2;
